2bea2fe23c535d9d137366d3c8d14138deae5da2
dluman <dluman@allegheny.edu>
2023-06-21 15:31:34 -0400

    Escaping quoted strings

--- chompchainwallet/transaction.py
+++ chompchainwallet/transaction.py
@@ -1,5 +1,7 @@
+import os
 import json
 import hashlib
+import requests
 from datetime import datetime
 from .interface import Wallet
 
@@ -11,6 +13,10 @@ class Transaction:
         wallet = Wallet()
 
         self.data = kwargs
+
+        for field in self.data:
+            self.data[field] = self.data[field].encode().decode()
+
         setattr(self, "to_addr", to_addr)
 
         if not from_addr:
@@ -31,3 +37,20 @@ class Transaction:
 
     def __str__(self):
         return json.dumps(self.__dict__, separators = (',', ':'))
+
+class CmdTransaction():
+
+    def __init__(self, command: str = ""):
+        transact = Transaction(
+            to_addr = "0x0",
+            cmd = command
+        )
+        transmit(transact)
+
+def transmit(txn: Transaction = Transaction()):
+    response = requests.post(
+        "http://cdr.theterm.world:7500/transactions/new",
+        data = json.dumps(txn.__dict__)
+    )
+    # TODO: Create a cache to save transactions in case
+    # we lose all connectivity with any node?
